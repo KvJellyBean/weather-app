4bf7de36394ef0a093aec42e28a577d375e254ff
KvJellyBean <kevinnatan91@gmail.com>
2024-01-26 12:30:57 +0700

    Create the website's structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+import Header from './modules/header';
+import MainPage from './modules/mainPage';
+import Footer from './modules/footer';
 import './style.css';
+
+function initApp() {
+  const bodyTag = document.body;
+  const headerTag = document.createElement('header');
+  const mainTag = document.createElement('main');
+  const footerTag = document.createElement('footer');
+  bodyTag.append(headerTag, mainTag, footerTag);
+}
+
+function loadDisplay() {
+  initApp();
+  Header.initHeader();
+  MainPage.initMain();
+  Footer.initFooter();
+}
+
+loadDisplay();
