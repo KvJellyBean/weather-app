e231b7db4347e0f9648f79bda8b1a4195ce83c33
KvJellyBean <kevinnatan91@gmail.com>
2024-01-30 22:56:11 +0700

    Add new DOM manipulation and functions
    Add screen functionality to show or render each sections, get image path, display unit base on the data

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,21 +1,23 @@
-import { fromUnixTime, format } from 'date-fns'; // Import from date-fns package module
+import { fromUnixTime, format, getHours } from 'date-fns'; // Import from date-fns package module
 import { utcToZonedTime } from 'date-fns-tz'; // Import from date-fns-tz package module
 import GeoCode from './geocodeApi';
 import Weather from './weatherApi';
+import LocalStorage from './localStorage';
 
 const DOM = (() => {
   /**
    * Display geocode error message.
-   * @param {number} geocodeErrorData - The geocode error data, where -1 indicates city error and 0 indicates server error.
+   * @param {number} geocodeErrorCode - The geocode error code, where -1 indicates city error and 0 indicates server error.
    */
-  function showGeocodeError(geocodeErrorData) {
-    const messageTemplate =
-      geocodeErrorData === -1
+  function showGeocodeError(geocodeErrorCode) {
+    const errorMessage =
+      geocodeErrorCode === -1
         ? document.querySelector('.cityError')
         : document.querySelector('.serverError');
-    messageTemplate.classList.remove('hidden');
+
+    errorMessage.classList.remove('hidden');
     setTimeout(() => {
-      messageTemplate.classList.add('hidden');
+      errorMessage.classList.add('hidden');
     }, 2000);
   }
 
@@ -46,6 +48,28 @@ const DOM = (() => {
     allCelsiusComponents.forEach((component) => {
       component.classList[desiredUnit === 'metric' ? 'remove' : 'add']('hidden');
     });
+
+    // Load old data and save new data
+    LocalStorage.loadData();
+    const lastSavedData = LocalStorage.getSavedData();
+    lastSavedData.settings.unit = desiredUnit;
+    LocalStorage.saveData(lastSavedData);
+  }
+
+  // Show weather section
+  function showWeatherScreen() {
+    const weatherWrapper = document.querySelector('#weather');
+    const forecastWrapper = document.querySelector('#forecast');
+    weatherWrapper.classList.remove('hidden');
+    forecastWrapper.classList.add('hidden');
+  }
+
+  // Show forecast section
+  function showForecastScreen() {
+    const weatherWrapper = document.querySelector('#weather');
+    const forecastWrapper = document.querySelector('#forecast');
+    weatherWrapper.classList.add('hidden');
+    forecastWrapper.classList.remove('hidden');
   }
 
   /**
@@ -65,7 +89,7 @@ const DOM = (() => {
    * @returns {string} - The source path of the weather icon.
    */
   function getWeatherIcon(iconId) {
-    // Import all files in a folder (r = required.context folder)
+    // Import all files in image folder (r = required.context folder)
     const folder = require.context('../images', false, /\.(png|jpe?g|svg)$/);
     const weatherIconFiles = folder.keys().map(folder);
 
@@ -106,6 +130,32 @@ const DOM = (() => {
   }
 
   /**
+   * Get the source path of condition icon from the /images/icon folder based on the condition.
+   * @param {string} condition - The weather condition.
+   * @returns {string} - The source path of the condition icon.
+   */
+  function getConditionIcon(condition) {
+    // Import all files in icon folder
+    const folder = require.context('../images/icon', false, /\.(png|jpe?g|svg)$/);
+    const conditionIconFiles = folder.keys().map(folder);
+
+    switch (condition.toLowerCase()) {
+      case 'feels like':
+        return conditionIconFiles[0];
+      case 'humidity':
+        return conditionIconFiles[1];
+      case 'pressure':
+        return conditionIconFiles[2];
+      case 'wind speed':
+        return conditionIconFiles[4];
+      case 'uv index':
+        return conditionIconFiles[3];
+      default:
+        return '';
+    }
+  }
+
+  /**
    * Create an element for a weather condition.
    * @param {string} condition - The particular condition.
    * @param {number} dataCelsius - The data in metric unit / Celsius.
@@ -129,6 +179,9 @@ const DOM = (() => {
 
     element.classList.add('conditionInfoWrapper');
     element.innerHTML = `
+      <div class="icon">
+        <img src=${getConditionIcon(condition)}  alt="${condition} icon" />
+      </div>
       <p>${condition}</p>
       <p class="celsius ${dataCelsiusDisplay}">${dataCelsius}${metricUnit}</p>
       <p class="fahrenheit ${dataFahrenheitDisplay}">${dataFahrenheit}${imperialUnit}</p>
@@ -158,14 +211,39 @@ const DOM = (() => {
 
     list.innerHTML = `
       <span>${time}</span>
-      <img src=${getWeatherIcon(data.weather[0].icon)} alt="Weather Icon Information" />
+      <img src=${getWeatherIcon(data.weather[0].icon)} alt="Icon ${data.weather[0].description} at ${time}" />
       <p>${data.temp.max ? `${Math.round(data.temp.max)}&deg` : Math.round(data.temp)}${data.temp.min ? ` / ${Math.round(data.temp.min)}` : ''}&deg ${unit === 'metric' ? 'C' : 'F'}</p>
       ${hasDesctiption ? `<p>${data.weather[0].description}</p>` : ''}
     `;
     return list;
   }
 
+  /**
+   * Check the time, whether it's night or not
+   * @param {string} timestamp - Time in unix format.
+   * @param {string} timezone - The timezone for the weather data.
+   * @returns {string} - The source path of the condition icon.
+   */
+  function isNight(timestamp, timezone) {
+    const currentHour = getHours(getTime(fromUnixTime(+timestamp), timezone));
+    return currentHour >= 16;
+  }
+
   // ==================== RENDER FUNCTIONS ====================
+  /**
+   * Render the temperature unit based on unit settings on local storage.
+   * @param {string} unit - The unit for temperature measurement (metric/imperial).
+   */
+  function renderUnit(unit) {
+    const celsiusRadio = document.querySelector('#celsiusRadio');
+    const fahrenheitRadio = document.querySelector('#fahrenheitRadio');
+
+    if (unit === 'metric') {
+      celsiusRadio.checked = true;
+    } else {
+      fahrenheitRadio.checked = true;
+    }
+  }
 
   /**
    * Render the main/core information of the weather.
@@ -173,25 +251,39 @@ const DOM = (() => {
    * @param {object} weatherData - The weather data.
    * @param {string} unit - The unit for temperature measurement (metric/imperial).
    */
-  function renderCoreInfo(geocodeData, weatherData, unit) {
-    const coreInfoWrapper = document.querySelector('#coreInfo');
+  function renderMainInfo(geocodeData, weatherData, unit) {
+    const mainInfoWrapper = document.querySelector('#weather');
     const celsiusData = weatherData.dataCelsius;
     const fahrenheitData = weatherData.dataFahrenheit;
 
-    coreInfoWrapper.innerHTML = '';
-    coreInfoWrapper.innerHTML = `
-      <span>
-        ${format(
-          getTime(fromUnixTime(+celsiusData.current.dt), celsiusData.timezone),
-          'EEE hh:mm aa',
-          { timeZone: celsiusData.timezone }
-        )}
-      </span>
-      <h2 class="location">${geocodeData.name}, ${geocodeData.country}</h2>    
-      <h2 ${unit === 'metric' ? 'class="celsius"' : 'class="celsius hidden"'}>${Math.round(celsiusData.current.temp)}&deg C</h2>    
-      <h2 ${unit === 'metric' ? 'class="fahrenheit hidden"' : 'class="fahrenheit"'}>${Math.round(fahrenheitData.current.temp)}&deg F</h2>    
-      <img src=${getWeatherIcon(celsiusData.current.weather[0].icon)} alt="Weather Icon Information" />
-      <p>${celsiusData.current.weather[0].description}</p>
+    mainInfoWrapper.innerHTML = '';
+    mainInfoWrapper.innerHTML = `
+      <div id="mainContent">
+        <img src=${getWeatherIcon(celsiusData.current.weather[0].icon)} alt="${celsiusData.current.weather[0].description}" id="mainImg" class="icon${celsiusData.current.weather[0].icon}"/>
+        
+        <div id="mainInfo">
+          <div class="cityInfo">
+            <h2 class="location">${geocodeData.name}, ${geocodeData.country}</h2>    
+            <span>
+              ${format(
+                getTime(fromUnixTime(+celsiusData.current.dt), celsiusData.timezone),
+                'EEE hh:mm aa',
+                { timeZone: celsiusData.timezone }
+              )}
+            </span>
+          </div>
+
+          <div class="weatherInfo">
+            <h2 ${unit === 'metric' ? 'class="celsius"' : 'class="celsius hidden"'}>${Math.round(celsiusData.current.temp)}<sup>&deg C</sup></h2>    
+            <h2 ${unit === 'metric' ? 'class="fahrenheit hidden"' : 'class="fahrenheit"'}>${Math.round(fahrenheitData.current.temp)}<sup>&deg F</sup></h2>    
+            <p>${celsiusData.current.weather[0].description}</p>
+          </div>
+        </div>
+      </div>
+
+      <div id="sideContent">
+          
+      </div>
     `;
   }
 
@@ -242,11 +334,9 @@ const DOM = (() => {
    * @param {string} unit - The unit for temperature measurement (metric/imperial).
    */
   function renderCurrentCondition(weatherData, unit) {
-    const conditionInfoWrapper = document.querySelector('#conditionInfo');
+    const conditionInfoWrapper = document.querySelector('#sideContent');
     const currentCelsiusData = weatherData.dataCelsius.current;
     const currentFahrenheitData = weatherData.dataFahrenheit.current;
-
-    conditionInfoWrapper.innerHTML = '';
 
     // Condition Info
     const conditionElements = [
@@ -282,31 +372,7 @@ const DOM = (() => {
         ' m/s',
         ' mph'
       ),
-      createConditionElement('UV Index', currentCelsiusData.uvi, currentFahrenheitData.uvi, unit),
-      createConditionElement(
-        'Visibility',
-        currentCelsiusData.visibility,
-        currentFahrenheitData.visibility,
-        unit,
-        'm',
-        'm'
-      ),
-      createConditionElement(
-        'Dew Point',
-        currentCelsiusData.dew_point,
-        currentFahrenheitData.dew_point,
-        unit,
-        '&deg C',
-        '&deg F'
-      ),
-      createConditionElement(
-        'Cloudiness',
-        currentCelsiusData.clouds,
-        currentFahrenheitData.clouds,
-        unit,
-        '%',
-        '%'
-      )
+      createConditionElement('UV Index', currentCelsiusData.uvi, currentFahrenheitData.uvi, unit)
     ];
 
     conditionElements.forEach((element) => {
@@ -321,23 +387,42 @@ const DOM = (() => {
    */
   async function renderAll(cityName, unit = 'metric') {
     try {
-      DOM.showLoader();
+      showLoader();
+
+      // Fetch Geocode data
       const geocodeData = await GeoCode.getGeocode(cityName);
       if (geocodeData === -1 || geocodeData === 0) {
         showGeocodeError(geocodeData);
         return;
       }
 
-      const weatherData = await Weather.getWeather(geocodeData.lon, geocodeData.lat);
-      console.log(geocodeData);
-      console.log(weatherData.dataCelsius);
-      console.log(weatherData.dataFahrenheit);
+      // Save the current data
+      LocalStorage.saveData({
+        city: {
+          cityName,
+          lon: geocodeData.lon,
+          lat: geocodeData.lat
+        },
+        settings: {
+          unit
+        }
+      });
 
-      renderCoreInfo(geocodeData, weatherData, unit);
-      renderWeatherForecast('hourly', weatherData, unit, false);
+      // Fetch Weather data
+      const weatherData = await Weather.getWeather(geocodeData.lon, geocodeData.lat);
+
+      renderMainInfo(geocodeData, weatherData, unit);
       renderCurrentCondition(weatherData, unit);
+      renderWeatherForecast('hourly', weatherData, unit, false);
       renderWeatherForecast('daily', weatherData, unit, true);
+
+      if (isNight(weatherData.dataCelsius.current.dt, weatherData.dataCelsius.timezone)) {
+        document.body.classList.add('night');
+      } else {
+        document.body.classList.remove('night');
+      }
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.log('An error occured: ', error);
     } finally {
       hideLoader();
@@ -346,8 +431,10 @@ const DOM = (() => {
 
   return {
     showLoader,
-    hideLoader,
     showUnit,
+    showWeatherScreen,
+    showForecastScreen,
+    renderUnit,
     renderAll
   };
 })();
